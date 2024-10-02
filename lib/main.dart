a01689386662bbc7584006ce18344b8cd6f85ad9
husainarib <husainarib@gmail.com>
2024-10-02 09:10:32 -0400

    implementation of main UI of calculator answer display

--- lib/main.dart
+++ lib/main.dart
@@ -84,5 +84,100 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
-    //TO implement
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 184, 223, 241),
+      appBar: AppBar(
+        title: const Text('Calculator'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          width: 400,
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    //last input text
+                    Text(
+                      lastInput,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    //main display text
+                    Text(
+                      displayText,
+                      style: const TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              //TODO Numbers and Operators Buttons
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget buildButton(String buttonText) {
+    Color buttonColor = Colors.blueAccent;
+    if (buttonText == 'C') {
+      buttonColor = Colors.redAccent;
+    } else if (buttonText == '=') {
+      buttonColor = Colors.green;
+    }
+
+    return GestureDetector(
+      onTap: () {
+        if (buttonText == 'C') {
+          clear();
+        } else if (buttonText == '=') {
+          calculate();
+        } else if ('+-*/'.contains(buttonText)) {
+          operatorPressed(buttonText);
+        } else {
+          numberPressed(buttonText);
+        }
+      },
+      child: Container(
+        height:50,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
