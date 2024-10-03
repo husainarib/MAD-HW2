c77f9d1ad24882654d3c4e017089a516285b5c74
husainarib <husainarib@gmail.com>
2024-10-03 08:06:25 -0400

    Add comments for better code readability and apply box shadow to calculator buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(CalculatorApp());
 }
 
+// Main Application class that initialize the calculator widget
 class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,13 +20,15 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  // Variables for calculator state
   String displayText = '0';
   String result = '0';
   String operator = '';
-  double firstOperand = 0;
-  double secondOperand = 0;
+  double firstOp = 0;
+  double secondOp = 0;
   String lastInput = '';
 
+  // method to capture number presses
   void numberPressed(String number) {
     setState(() {
       if (displayText == '0') {
@@ -36,46 +39,49 @@ class _CalculatorState extends State<Calculator> {
     });
   }
 
+  // method to capture operator presses
   void operatorPressed(String op) {
     setState(() {
       lastInput = displayText;
-      firstOperand = double.parse(displayText);
+      firstOp = double.parse(displayText);
       operator = op;
       displayText = '';
     });
   }
 
+  // method to calculate
   void calculate() {
     setState(() {
-      secondOperand = double.parse(displayText);
+      secondOp = double.parse(displayText);
       switch (operator) {
         case '+':
-          result = (firstOperand + secondOperand).toString();
+          result = (firstOp + secondOp).toString();
           break;
         case '-':
-          result = (firstOperand - secondOperand).toString();
+          result = (firstOp - secondOp).toString();
           break;
         case '*':
-          result = (firstOperand * secondOperand).toString();
+          result = (firstOp * secondOp).toString();
           break;
         case '/':
-          if (secondOperand != 0) {
-            result = (firstOperand / secondOperand).toString();
+          if (secondOp != 0) {
+            result = (firstOp / secondOp).toString();
           } else {
-            result = 'Error';
+            result = 'ERROR';
           }
           break;
       }
       displayText = result;
-      lastInput = '$firstOperand $operator $secondOperand';
+      lastInput = '$firstOp $operator $secondOp';
     });
   }
 
+  // method to reset all variable values
   void clear() {
     setState(() {
       displayText = '0';
-      firstOperand = 0;
-      secondOperand = 0;
+      firstOp = 0;
+      secondOp = 0;
       operator = '';
       result = '0';
       lastInput = '';
@@ -102,10 +108,11 @@ class _CalculatorState extends State<Calculator> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // Display screen for calculator
               Container(
                 alignment: Alignment.centerRight,
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+                    const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
                 decoration: BoxDecoration(
                   color: Colors.green,
                   borderRadius: BorderRadius.circular(10),
@@ -134,7 +141,7 @@ class _CalculatorState extends State<Calculator> {
                 ),
               ),
               const SizedBox(height: 10),
-              // Numbers and Operators Buttons
+              // Main display for number pad and operators
               Column(
                 children: [
                   buildButtonRow(['7', '8', '9', '/']),
@@ -150,7 +157,7 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 
-//calculator number pad
+  //calculator number pad widget
   Widget buildButtonRow(List<String> buttons) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -165,7 +172,7 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 
-//button  
+  // button widget
   Widget buildButton(String butonInput) {
     Color buttonColor = const Color.fromARGB(255, 59, 137, 61);
     if (butonInput == 'C') {
@@ -174,6 +181,7 @@ class _CalculatorState extends State<Calculator> {
       buttonColor = Colors.blueAccent;
     }
 
+    // method for registering user taps
     return InkWell(
       onTap: () {
         if (butonInput == 'C') {
@@ -191,6 +199,14 @@ class _CalculatorState extends State<Calculator> {
         decoration: BoxDecoration(
           color: buttonColor,
           borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 6,
+              offset: const Offset(2, 4),
+            ),
+          ],
         ),
         child: Center(
           child: Text(
